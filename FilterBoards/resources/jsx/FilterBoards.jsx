eced78435202dc44a6076638adf5ad8e966526a0
nibir2010 <nibir.datta2010@gmail.com>
2017-05-07 14:11:36 +0530

    Update FilterBoards.jsx
    some changes

--- FilterBoards/resources/jsx/FilterBoards.jsx
+++ FilterBoards/resources/jsx/FilterBoards.jsx
@@ -8,7 +8,7 @@ export default class FilterBoards extends React.Component {
 		this.state = {
 			list:{
 				'SSLC':['img1','img5','img6','img8','img9','img11','img12'],
-				'CBCS':['img3','img7'],
+				'CBSE':['img3','img7'],
 				'ICSE':['img2','img13'],
 				'PU':['img10','img12'],
 				'Degree':['img4','img15']
